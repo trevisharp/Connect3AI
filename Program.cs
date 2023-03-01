3f5b7936cfea08b6318c3b5c625bd342a7971761
Trevisan <38115923+trevisharp@users.noreply.github.com>
2023-03-01 16:35:23 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,24 +2,13 @@ using System.Drawing;
 using System.Windows.Forms;
 
 Connect3State state = new Connect3State();
-state = new Connect3State(state, 0, Piece.White);
-
-state = new Connect3State(state, 1, Piece.Black);
-state = new Connect3State(state, 0, Piece.Black);
-
-state = new Connect3State(state, 0, Piece.White);
-state = new Connect3State(state, 1, Piece.White);
-state = new Connect3State(state, 2, Piece.White);
-
-state = new Connect3State(state, 0, Piece.Black);
-state = new Connect3State(state, 1, Piece.Black);
-state = new Connect3State(state, 2, Piece.Black);
-state = new Connect3State(state, 3, Piece.Black);
+Piece piece = Piece.White;
 
 int frame = 0;
 Bitmap bmp = null;
 Graphics g = null;
 bool isDown = false;
+bool inDown = false;
 PointF cursor = PointF.Empty;
 
 ApplicationConfiguration.Initialize();
@@ -92,8 +81,10 @@ void draw()
     {
         for (int i = 0; i < 4; i++)
         {
-            RectangleF rect = new RectangleF(desloc + size * (i + 1), size * (j + 1), 3 * size / 4, 3 * size / 4);
+            int x = desloc + size * (i + 1);
+            RectangleF rect = new RectangleF(x, size * (j + 1), 3 * size / 4, 3 * size / 4);
             var piece = state[i, 3 - j];
+
             switch (piece)
             {
                 case Piece.Black:
@@ -107,5 +98,55 @@ void draw()
             }
             g.DrawEllipse(Pens.Black, rect);
         }
+    }
+
+    for (byte i = 0; i < 4; i++)
+    {
+        int x = desloc + size * (i + 1);
+        if (cursor.X < x || cursor.X > x + size)
+            continue;
+        
+        var j = 3 - state.GetLevel(i);
+        if (j > 3)
+            return;
+        
+        RectangleF rect = new RectangleF(x, size * (j + 1), 3 * size / 4, 3 * size / 4);
+        switch (piece)
+        {
+            case Piece.Black:
+                var brush = new SolidBrush(Color.FromArgb(128, 255, 0, 0));
+                g.FillEllipse(brush, rect);
+                break;
+            
+            case Piece.White:
+                var brush2 = new SolidBrush(Color.FromArgb(128, 0, 0, 255));
+                g.FillEllipse(brush2, rect);
+                break;
+
+        }
+
+        if (inDown && !isDown)
+        {
+            state = new Connect3State(state, i, piece);
+            switch (state.CheckWin())
+            {
+                case Piece.White:
+                    g.Clear(Color.Blue);
+                    tm.Stop();
+                    break;
+                
+                case Piece.Black:
+                    g.Clear(Color.Red);
+                    tm.Stop();
+                    break;
+            }
+            piece = piece == Piece.White ? Piece.Black : Piece.White;
+            inDown = false;
+            return;
+        }
+
+        if (!inDown && isDown)
+            inDown = true;
+
     }
 }
